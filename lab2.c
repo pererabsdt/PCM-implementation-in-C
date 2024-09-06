8546aace95e5f464980c1d601d4fe62f1202761a
Dulitha Perera <dulitha.22@cse.mrt.ac.lk>
2024-09-06 16:57:01 +0530

    Implement the run function

--- lab2.c
+++ lab2.c
@@ -14,15 +14,40 @@ void run(asignal * inputsignal);
 int main() {
     asignal * inputsignal = (asignal *) malloc(sizeof(asignal));
     run(inputsignal);
-
-    call any other function here
-
     free(inputsignal);
     return 0;
 }
 
 void run(asignal* inputsignal) {
+    float A, omega, sigma;
+    int duration, interval, encoderbits;
+    scanf("%f %f %f %d %d %d", &A, &omega, &sigma, &duration, &interval, &encoderbits);
 
+    inputsignal->A = A;
+    inputsignal->sigma = sigma;
+    inputsignal->omega = omega;
+    inputsignal->duration = duration;
+
+    int levels = pow(2, encoderbits);
+    int array_size = duration / interval + 2;
+
+    float sampleArray[array_size];
+    sampler(sampleArray, interval, *inputsignal);
+
+    int pcmpulsesArray[array_size];
+    quantizer(sampleArray, pcmpulsesArray, levels, A);
+
+    int number_of_bits = 2 * (array_size - 1);
+    int dsignalArray[number_of_bits + 1];
+
+    encoder(pcmpulsesArray, dsignalArray, encoderbits);
+
+    for (int i = 1; i < number_of_bits + 1; ++i) {
+        printf("%d", dsignalArray[i]);
+    }
 }
+
+
+
 
 
